886efd97bd307f0a5d56c3bc7e24bf3be1f83c7a
Adrian Herridge <adrian@herridges.com>
2022-02-21 15:13:41 +0000

    created separate postgres provider

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwitchbladePostgres",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SwitchbladePostgres",
+            targets: ["SwitchbladePostgres"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/sharksync/switchblade.git", .branch("master")),
+        .package(url: "https://github.com/vapor/postgres-kit.git", .exact("2.3.3")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git",   .upToNextMinor(from: "1.3.8")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "SwitchbladePostgres",
+            dependencies: [.product(name: "Switchblade", package: "switchblade"), .product(name: "PostgresKit", package: "postgres-kit", condition: .when(platforms: [.linux,.macOS])),"CryptoSwift"]),
+        .testTarget(
+            name: "SwitchbladePostgresTests",
+            dependencies: ["SwitchbladePostgres"]),
+    ]
+)
